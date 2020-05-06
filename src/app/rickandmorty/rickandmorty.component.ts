2a6e57a5956a5aeedc2db596c2a2a26abbff447d
unknown <marcin080599@gmail.com>
2020-05-06 14:14:18 +0200

    first add

---
+++ src/app/rickandmorty/rickandmorty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rickandmorty',
+  templateUrl: './rickandmorty.component.html',
+  styleUrls: ['./rickandmorty.component.scss']
+})
+export class RickandmortyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
